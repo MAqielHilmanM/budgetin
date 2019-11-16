4ff790b0ffe1d6ee16fa7b47e901b18b1898a26d
Indra Deva <indra.d373@gmail.com>
2019-11-16 18:16:28 +0700

    edit statusbar, change text widget to custom TextWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
-import 'package:budgetin/screens/dashboard/DashboardPage.dart';
 import 'package:budgetin/screens/onboarding/onboardingScreen.dart';
 import 'package:budgetin/screens/splashscreen/SplashScreen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
-
+void main() { 
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent, //top bar color
+      statusBarIconBrightness: Brightness.dark, //top bar icons
+      systemNavigationBarColor: Colors.white, //bottom bar color
+      systemNavigationBarIconBrightness: Brightness.dark, //bottom bar icons
+    )
+  );
+  runApp(MyApp());
+}
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
