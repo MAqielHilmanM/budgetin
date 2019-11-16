e265b430ffaf04a5f21093ea4f97b4a71fc4ccad
M Aqiel Hilman M <maqiel2600@gmail.com>
2019-11-16 14:11:10 +0700

    prepare asset and fix name

---
+++ lib/utils/Assets.dart
@@ -0,0 +1,21 @@
+class Assets{
+  static const String iconArrowDown = "assets/images/ic_arrow_down/ic_arrow_down.png";
+  static const String iconArrowLeft =  "assets/images/ic_arrow_left/ic_arrow_left.png";
+  static const String iconArrowRight =  "assets/images/ic_arrow_right/ic_arrow_right.png";
+  static const String iconArrowUp =  "assets/images/ic_arrow_up/ic_arrow_up.png";
+  static const String iconAuth =  "assets/images/ic_auth/ic_auth.png";
+  static const String iconClose =  "assets/images/ic_close/ic_close.png";
+  static const String iconDrawer =  "assets/images/ic_drawer/ic_drawer.png";
+  static const String iconLogo =  "assets/images/ic_logo/ic_logo.png";
+  static const String iconMenuBudget =  "assets/images/ic_menu_budget/ic_menu_budget.png";
+  static const String iconMenuHome =  "assets/images/ic_menu_home/ic_menu_home.png";
+  static const String iconMenuInvest =  "assets/images/ic_menu_invest/ic_menu_invest.png";
+  static const String iconMenuLogout =  "assets/images/ic_menu_logout/ic_menu_logout.png";
+  static const String iconMenuProfile =  "assets/images/ic_menu_profile/ic_menu_profile.png";
+  static const String iconMenuTransaction =  "assets/images/ic_menu_transaction/ic_menu_transaction.png";
+  static const String iconOnboard1 =  "assets/images/ic_onboard1/ic_onboard1.png";
+  static const String iconOnboard2 =  "assets/images/ic_onboard2/ic_onboard2.png";
+  static const String iconOnboard3 =  "assets/images/ic_onboard3/ic_onboard3.png";
+  static const String iconPlusBlue =  "assets/images/ic_plus_blue/ic_plus_blue.png";
+  static const String iconPlusEhite =  "assets/images/ic_plus_white/ic_plus_white.png";
+}
--- lib/widget/Theme.dart
+++ lib/widget/Theme.dart
@@ -6,4 +6,5 @@ class CustomTheme {
   static const Color colorRed = const Color(0xFFF92C2C);
   static const Color colorGreen = const Color(0xFF1DF2A6);
   static const Color colorBlueDark = const Color(0xFF4242AF);
+
 }
