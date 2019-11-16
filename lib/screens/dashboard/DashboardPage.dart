ed0502ca8f28961ad4cace213a361ae71bb1700b
M Aqiel Hilman M <maqiel2600@gmail.com>
2019-11-16 14:31:03 +0700

    prepare boilerplate

---
+++ lib/screens/dashboard/DashboardPage.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class DashboardPage extends StatefulWidget {
+  DashboardPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _DashboardPageState createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text("Hello World")
+      )
+    );
+  }
+}
