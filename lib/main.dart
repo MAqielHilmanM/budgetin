8ac531fe2e3bc0e35bd402d52851da655481ac84
M Aqiel Hilman M <maqiel2600@gmail.com>
2019-11-17 00:09:27 +0700

    add screen Utils and size

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,19 @@
+import 'package:budgetin/screens/login/LoginPage.dart';
 import 'package:budgetin/screens/onboarding/onboardingScreen.dart';
 import 'package:budgetin/screens/splashscreen/SplashScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() { 
-  SystemChrome.setSystemUIOverlayStyle(
-    SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent, //top bar color
-      statusBarIconBrightness: Brightness.dark, //top bar icons
-      systemNavigationBarColor: Colors.white, //bottom bar color
-      systemNavigationBarIconBrightness: Brightness.dark, //bottom bar icons
-    )
-  );
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent, //top bar color
+    statusBarIconBrightness: Brightness.dark, //top bar icons
+    systemNavigationBarColor: Colors.white, //bottom bar color
+    systemNavigationBarIconBrightness: Brightness.dark, //bottom bar icons
+  ));
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -25,9 +25,9 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: new SplashScreen(),
-      routes: <String, WidgetBuilder> {
-    '/screens/onboarding': (BuildContext context) => new DashboardPage()
-  },
+      routes: <String, WidgetBuilder>{
+        '/screens/onboarding': (BuildContext context) => OnBoardingScreen(),
+      },
     );
   }
 }
